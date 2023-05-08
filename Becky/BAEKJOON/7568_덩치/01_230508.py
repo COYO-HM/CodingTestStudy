89316111423e9ed4da9c82e5e06f28d93084b29c
psrom <psrom721@gmail.com>
2023-05-08 21:35:55 +0900

    BAEKJOON: 7568

---
+++ Becky/BAEKJOON/7568_덩치/01_230508.py
@@ -0,0 +1,15 @@
+# 문제: https://www.acmicpc.net/problem/7568
+n = int(input())
+lst = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    lst.append((x, y))
+
+for i in lst:
+    rank = 1
+    for j in lst:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end = " ")
+
