481c43084eb97b32ae51c5ea91dea702b4556a2b
psrom <psrom721@gmail.com>
2023-05-23 22:30:58 +0900

    BOJ: 1205

---
+++ Becky/BAEKJOON/1205_등수 구하기/01_230523.py
@@ -0,0 +1,16 @@
+#문제: https://www.acmicpc.net/problem/1205
+n, new, p = map(int, input().split())
+
+if n == 0:
+    print(1)
+else:
+    score = list(map(int, input().split()))
+    if n == p and score[-1] >= new:
+        print(-1)
+    else:
+        rank = n+1
+        for i in range(n):
+            if score[i] <= new:
+                rank = i+1
+                break
+        print(rank)
