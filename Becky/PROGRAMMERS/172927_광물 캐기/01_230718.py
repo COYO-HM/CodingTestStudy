ddccd96c3424f08f0662cb7a3425d74fc8fd5d24
psrom <psrom721@gmail.com>
2023-07-19 00:07:43 +0900

    ADD: annotation

--- Becky/PROGRAMMERS/172927_광물 캐기/01_230718.py
+++ Becky/PROGRAMMERS/172927_광물 캐기/01_230718.py
@@ -1,19 +1,19 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/172927
+# https://school.programmers.co.kr/learn/courses/30/lessons/172927
 def solution(picks, minerals):
     # 5개씩 광물 묶기
     # 곡괭이를 모두 사용해도 광물이 남은 경우를 처리하기 위해 min 함수 사용
     minerals = [list(minerals[i:i+5]) for i in range(0, min(sum(picks) * 5, len(minerals)), 5)]
     scores = []
     
+    # 곡괭이 종류에 따른 광물 피로도 계산
     for m in minerals:
         tmp = [0, 0, 0]
-        
         for i in m:
             tmp[0] += 1
             tmp[1] += 5 if i == "diamond" else 1
             tmp[2] += 25 if i == "diamond" else 5 if i == "iron" else 1
         scores.append(tmp)
-    
+    # 내림차순 정렬
     scores.sort(key=lambda x:(-x[2],-x[1]))
     
     answer = 0
