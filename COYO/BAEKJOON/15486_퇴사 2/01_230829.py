dc0936804748d82676f7c9c797ae77eb1bb93c37
COYO <dayeon.yoon@gmail.com>
2023-08-31 01:59:24 +0900

    BOJ: 퇴사 2

--- COYO/BAEKJOON/15486_퇴사 2/01_230829.py
+++ COYO/BAEKJOON/15486_퇴사 2/01_230829.py
@@ -1,8 +1,15 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-chart = [map(int, input().split()) for _ in range(n)]
-dp = [0] * n
-for i, [t, p] in enumerate(chart):
+chart = [map(int, input().split()) for i in range(n)]
+dp = [0] * (n + 2)
 
+for i in range(n, 0, -1):
+    [t, p] = chart[i - 1]
+    # print(i, dp)
+    if i + t <= n + 1:
+        dp[i] = max(dp[i + 1], dp[i + t] + p)
+    else:
+        dp[i] = dp[i + 1]
 
+print(dp[1])
