c9f1125f4c82a86b40f6567a310e8ad9938866d7
psrom <psrom721@gmail.com>
2023-10-31 18:47:07 +0900

    refactor(BOJ_줄세우기): 사용하지 않는 stack 삭제

--- Becky/BAEKJOON/10431_줄세우기/01_231031.py
+++ Becky/BAEKJOON/10431_줄세우기/01_231031.py
@@ -4,7 +4,6 @@ for _ in range(P):
     students = list(map(int, input().split()))
     tc_number = students.pop(0)  # 첫 번째로 입력 받은 숫자가 tc 번호
 
-    stack = []
     count_move = 0
     for i in range(1, 20):
         for j in range(i - 1, -1, -1):
