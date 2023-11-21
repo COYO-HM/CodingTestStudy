b3951c3d858ae413f766026b111b1f127aa4531e
psrom <psrom721@gmail.com>
2023-11-21 17:18:33 +0900

    refactor: PROGRAMMERS_118666 성격 유형 검사하기

--- Becky/KAKAO/2022_INTERNSHIP/118666_성격 유형 검사하기/01_231121.py
+++ Becky/KAKAO/2022_INTERNSHIP/118666_성격 유형 검사하기/01_231121.py
@@ -1,12 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118666
 def solution(survey, choices):
     kakao_type = ["R", "T", "C", "F", "J", "M", "A", "N"]
-    kakao_dic = dict()
-    for k_t in kakao_type:
-        kakao_dic[k_t] = 0
+    kakao_dic = {k_t: 0 for k_t in kakao_type}
 
-    for i in range(len(survey)):
-        disagree, agree = survey[i][0], survey[i][1]
+    for i, (disagree, agree) in enumerate(survey):
         choice = choices[i]
 
         if choice > 4:
@@ -14,25 +11,11 @@ def solution(survey, choices):
         elif choice < 4:
             kakao_dic[disagree] += 4 - choice
 
-    answer = ''
-    if kakao_dic["R"] >= kakao_dic["T"]:
-        answer += "R"
-    else:
-        answer += "T"
-
-    if kakao_dic["C"] >= kakao_dic["F"]:
-        answer += "C"
-    else:
-        answer += "F"
-
-    if kakao_dic["J"] >= kakao_dic["M"]:
-        answer += "J"
-    else:
-        answer += "M"
-
-    if kakao_dic["A"] >= kakao_dic["N"]:
-        answer += "A"
-    else:
-        answer += "N"
+    answer = ""
+    for first, second in [("R", "T"), ("C", "F"), ("J", "M"), ("A", "N")]:
+        if kakao_dic[first] >= kakao_dic[second]:
+            answer += first
+        else:
+            answer += second
 
     return answer
