0d482a07e6c8e7661d142b93a6b38d7ea81b880a
psrom <psrom721@gmail.com>
2023-12-15 23:00:03 +0900

    BOJ: 17484_진우의 달 여행(Small)

---
+++ Becky/BAEKJOON/17484_진우의 달 여행/01_231215.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/17484
+# 22:28-22:59
+from collections import deque
+
+
+def dfs(x, y, fuel, prev_direction):
+    global min_fuel
+
+    if x == N - 1:
+        min_fuel = min(min_fuel, fuel)
+        return
+
+    for dx, dy in [(1, -1), (1, 0), (1, 1)]:
+        nx = x + dx
+        ny = y + dy
+
+        if 0 <= nx < N and 0 <= ny < M and (dx, dy) != prev_direction:
+            dfs(nx, ny, fuel + graph[nx][ny], (dx, dy))
+
+
+N, M = map(int, input().split())
+graph = list(list(map(int, input().split())) for _ in range(N))
+
+min_fuel = float("inf")
+
+for j in range(M):
+    dfs(0, j, graph[0][j], (-1, -1))
+
+print(min_fuel)
