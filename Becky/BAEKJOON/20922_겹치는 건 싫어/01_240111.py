e87c8a0ee7346b671c7eb5abd6e6e30649c3a191
psrom <psrom721@gmail.com>
2024-01-11 11:40:17 +0900

    BOJ: 20922_겹치는 건 싫어

---
+++ Becky/BAEKJOON/20922_겹치는 건 싫어/01_240111.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/20922
+# 10:49-11:36
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+lst = list(map(int, input().rstrip().split()))
+l, r = 0, 0
+
+counter = [0] * (max(lst) + 1)
+answer = 0
+
+while r < N:
+    if counter[lst[r]] < K:
+        counter[lst[r]] += 1
+        r += 1
+    else:
+        counter[lst[l]] -= 1
+        l += 1
+    answer = max(answer, r - l)
+    print(r, l)
+
+print(answer)
