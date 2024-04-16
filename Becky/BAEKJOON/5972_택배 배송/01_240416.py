27238995a92955542192d42f4d22935041c5eda8
psrom <psrom721@gmail.com>
2024-04-16 17:26:16 +0900

    BOJ: 5972_택배 발송

---
+++ Becky/BAEKJOON/5972_택배 배송/01_240416.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/5972
+# 16:24-
+import heapq
+
+
+def dijkstra(start):
+    que = []
+    heapq.heappush(que, (0, start))  # 거리, 시작점
+    distance[start] = 0
+
+    while que:
+        d, curr = heapq.heappop(que)
+
+        if distance[curr] < d:
+            continue
+
+        for v, w in graph[curr]:
+            temp = d + w
+            if temp < distance[v]:
+                distance[v] = temp
+                heapq.heappush(que, (temp, v))
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [float("inf")] * (n + 1)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+dijkstra(1)
+print(distance[n])
